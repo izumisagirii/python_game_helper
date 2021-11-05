49752ae56ba4a0aa4dc15d52e2c212a37354b445
ew <53885068+izumisagirii@users.noreply.github.com>
2021-11-05 10:03:10 +0800

    add help

--- helper.py
+++ helper.py
@@ -5,4 +5,5 @@ def printhelp(content_text):
     content_text.AppendText("[help]--输出帮助\n")
     content_text.AppendText("[daily]--做每日（待做）\n")
     content_text.AppendText("[shot NAME]--截图，NAME为文件名称（无后缀）默认为png格式,存储位置程序文件夹\n")
+    content_text.AppendText("[genshin resin]--去风本无脑清体力\n")
     content_text.AppendText("不同命令可以使用&分隔，会按照顺序执行\n")
